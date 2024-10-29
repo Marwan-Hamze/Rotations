d9bc9245ca323aa4c209dc4efd42c3beec39cd02
Marwan-Hamze <marwan2905@hotmail.com>
2024-10-29 16:28:48 +0900

    Small Update

--- Rotations.py
+++ Rotations.py
@@ -4,17 +4,19 @@ import numpy as np
 
 # Declaring different variables representing different orientation notations
 
-quaternion = [math.pi/8, math.pi/8, 0, 0]
+quaternion = [1, 0, 0, 0]
 
-quaternion2 = [math.pi/4, math.pi/4, 0, 0]
+quaternion2 = [1, 0, 0, 0]
 
 quaternion2_conjugate = [quaternion2[0], -quaternion2[1], -quaternion2[2], -quaternion2[3]]
+
+quaternion4 = [0.5556245380733142, -0.5370365782417992, 0.38832455314425374, 0.5020728311176293]
 
 euler = tf3.euler.quat2euler(quaternion)
 
 euler2 = [0.0, -1.7, 0.0]
 
-euler3 = [-math.pi/2, 0.0, 0.0]
+euler3 = [math.pi/2, math.pi/2, math.pi/2]
 
 x = np.array([[1, 1, 1], [2, 2, 2], [1, 2, 3]])
 y = np.array([[1, 0, 0], [2, 0, 0], [1, 2, 3]])
@@ -27,7 +29,7 @@ quaternion3 = tf3.euler.euler2quat(euler3[0], euler3[1], euler3[2])
 
 rotmat_ref = tf3.euler.euler2mat(euler3[0], euler3[1], euler3[2])
 
-# print(rotmat_ref)
+# print(quaternion3)
 
 # print("Euler is {}".format(euler3))
 
@@ -67,6 +69,14 @@ e = tf3.euler.mat2euler(w)
 
 norm3 = np.linalg.norm(e)
 
-print (w)
-print (e)
-print (norm3)
+# print (w)
+# print (e)
+# print (norm3)
+
+# Difference in Quaternions just like in the RL project
+
+Diff = 1 * (1 - np.inner(quaternion, quaternion4) ** 2)
+
+print("This is the error in quaternions used in RL: {}".format(Diff))
+
+print("This is the reward of quaternions difference: {}".format(0.50 * np.exp(-Diff)))
